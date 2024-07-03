35e365c039c7593d9de7eee368ff1252b662094b
Martins Leitarts <martins.leitarts@kleintech.net>
2024-07-03 20:46:05 +0300

    first commit

---
+++ Day1/Day1/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        sample2();
+    }
+
+    public static void sample1(){
+        int a = 2;
+        String b = "aaa";
+        double c = 2.4;
+        boolean d = true;
+
+        int[] aa = new int[3];
+        int[] bb = {1,2,3};
+
+        List<Integer> cc = new ArrayList<>();
+        cc.add(1);
+        cc.add(4);
+
+        System.out.println(cc.get(0));
+    }
+
+    public static void sample2(){
+        System.out.println("Izvade");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadi tekstu!");
+
+        String input = sc.nextLine();
+        System.out.println(input);
+
+        try{
+            int input2 = sc.nextInt();
+        }catch(Exception e){
+            System.out.println("Ievadiet skaitli!");
+        }
+    }
+
+    public static void sample3(){
+        String a = "aaaa";
+
+        if(a.equals("aaaa")){
+
+        }
+    }
+}
